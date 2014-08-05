227f604d56d2e9377f277eb03a26614369a5f46c
raph <hpavavar@gmail.com>
2014-08-05 10:16:06 +0200

    + login and createUser

--- PrestashopBridge.php
+++ PrestashopBridge.php
@@ -54,12 +54,73 @@ class PrestashopBridge {
 	}
 
 
-	public function createUser($email, $lastname, $firstname, $password = null) {
+	public function login($email) {
+		$this->loadPrestaKernel();
 
+		$customer = new \Customer();
+		$authentication = $customer->getByEmail($email);
+
+		if (!$authentication) //user doesn't exist
+			return false;
+
+		$ctx = \Context::getContext();
+
+		$ctx->cookie->id_compare = isset($ctx->cookie->id_compare) ? $ctx->cookie->id_compare: \CompareProduct::getIdCompareByIdCustomer($customer->id);
+		$ctx->cookie->id_customer = (int)($customer->id);
+		$ctx->cookie->customer_lastname = $customer->lastname;
+		$ctx->cookie->customer_firstname = $customer->firstname;
+		$ctx->cookie->logged = 1;
+		$customer->logged = 1;
+		$ctx->cookie->is_guest = $customer->isGuest();
+		$ctx->cookie->passwd = $customer->passwd;
+		$ctx->cookie->email = $customer->email;
+
+		// Add customer to the context
+		$ctx->customer = $customer;
+
+		$id_cart = (int)\Cart::lastNoneOrderedCart($ctx->customer->id);
+		if ($id_cart) {
+			$ctx->cart = new \Cart($id_cart);
+		} else {
+			$ctx->cart = new \Cart();
+			$ctx->cart->id_currency = \Currency::getDefaultCurrency()->id; //mandatory field
+		}
+
+		$ctx->cart->id_customer = (int)$customer->id;
+		$ctx->cart->secure_key = $customer->secure_key;
+		$ctx->cart->save();
+		$ctx->cookie->id_cart = (int)$ctx->cart->id;
+
+		\CartRule::autoRemoveFromCart($ctx);
+		\CartRule::autoAddToCart($ctx);
+
+		$ctx->cookie->write();
+		return true;
 	}
 
-	public function login($email) {
+	/**
+	* @param string password  md5 string or null
+	* if password = null, login will only be possible by the current bridge
+	*/
+	public function createUser($email, $lastname, $firstname, $password = null) {
 
+		if (\Customer::customerExists($email)) {
+			return false;
+		}
+
+		$customer = new \Customer();
+
+		$customer->active = 1;
+		$customer->firstname = $firstname;
+		$customer->lastname = $lastname;
+		$customer->email = $email;
+		$customer->active = 1;
+		$customer->passwd  = $password ? $password : md5(bin2hex(openssl_random_pseudo_bytes(10)));
+
+		if ($customer->add())
+			return true;
+		else
+			return false;
 	}
 
 	public function logout() {
