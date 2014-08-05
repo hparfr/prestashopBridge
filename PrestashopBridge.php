545eafe14b050db32f0894f6d14fb79351a700c9
raph <hpavavar@gmail.com>
2014-08-05 10:07:45 +0200

    fix the $_SERVER['SERVER_NAME'] issue

--- PrestashopBridge.php
+++ PrestashopBridge.php
@@ -28,9 +28,14 @@ class PrestashopBridge {
 		//if id_shop is not found in $_GET or $_POST
 		//a redirection will be done in Prestashop/classes/shop/Shop.php:initialize()
 
-		//add id_shop in $_GET in a new HttpFundation\Request
-		$requestClean = Request::create('', 'GET', array('id_shop'=> $this->id_shop));
-		$requestClean->overrideGlobals();
+		//we need also $_SERVER['SERVER_NAME'] for setting the Cookie in the right domain
+		$currentRequest = Request::createFromGlobals();
+
+		//create new HttpFundation\Request
+		//add id_shop in $_GET
+		//copy $_SERVER from currentRequest
+		$cleanRequest = Request::create('', 'GET', array('id_shop'=> $this->id_shop), array(), array(), $currentRequest->server->all());
+		$cleanRequest->overrideGlobals();
 
 		//init prestashop
 		include($this->pathToPrestashop.'/config/config.inc.php');
